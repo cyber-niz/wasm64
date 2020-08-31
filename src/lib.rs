3455e83472134ec0e2373e502ff43ac709df783b
nizam0906 <nizamudheenak8@gmail.com>
2020-09-01 01:38:40 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub fn ec(cleartext: &str) -> String {
   let encoded = base64::encode(cleartext.as_bytes());
   return encoded;
 }
+#[wasm_bindgen]
 pub fn dc(cleartext: &str) -> String {
   let decoded = base64::decode(cleartext.as_bytes());
   return decoded;
