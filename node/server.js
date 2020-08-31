455378df51ad5859f47b00eeca675c420b0bdb0a
nizam0906 <nizamudheenak8@gmail.com>
2020-08-31 11:54:52 +0530

    Update and rename app.js to server.js

---
+++ node/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { encrypt } = require('../pkg/base64_lib.js');
+
+const app = express();
+const port = 8080;
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.redirect("/index.html"));
+
+app.post('/encode', function (req, res) {
+  var cleartext = (req.body.cleartext);
+  res.send(ec(cleartext))
+})
+
+app.post('/decode', function (req, res) {
+  var cleartext = (req.body.cleartext);
+  res.send(dc(cleartext))
+})
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+
