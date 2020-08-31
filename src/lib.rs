af64d72942ecc560b4e2df468d9b80c2bc0f8654
nizam0906 <nizamudheenak8@gmail.com>
2020-08-31 11:46:05 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+extern crate base64;
 use wasm_bindgen::prelude::*;
 
+// Encrypt to base64
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn ec(cleartext: &str) -> String {
+  let encoded = base64::encode(cleartext.as_bytes());
+  return encoded;
+}
+pub fn dc(cleartext: &str) -> String {
+  let decoded = base64::decode(cleartext.as_bytes());
+  return decoded;
 }
